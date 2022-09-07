8dfba25d5d8145adb750777e2f9eb0af17cb309b
marco-calautti <marco.calautti@gmail.com>
2022-09-07 12:41:19 +0200

    Fixed extern reference on windows

--- xdelta3_wrapper.cpp
+++ xdelta3_wrapper.cpp
@@ -8,13 +8,13 @@ extern "C"
     /**
     * Default command line like interface. The first parameter must be a dummy param for the executable name (e.g. xdelta 3)
     **/
-    int xd3_main_cmdline(int argc, char **argv);
+    int xd3_main_cmdline(int argc, char** argv);
+    extern void (*xprintf_message_func)(const char* msg);
 }
 
-extern void (*xprintf_message_func)(const char* msg);
 
 static std::string _messages;
-static void internal_printf(const char* msg){
+static void internal_printf(const char* msg) {
     _messages.append(msg);
 }
 
@@ -26,20 +26,20 @@ std::string xd3_messages()
 
 int xd3_main_exec(const std::vector<std::string>& params)
 {
-    char** argv = new char*[params.size()+2];
-    argv[0] = new char[8]{'x','d','e','l','t','a','3','\0'};
+    char** argv = new char* [params.size() + 2];
+    argv[0] = new char[8] {'x', 'd', 'e', 'l', 't', 'a', '3', '\0'};
 
     int count = 1;
-    for(auto& entry: params)
+    for (auto& entry : params)
     {
-        size_t len = entry.length()+1;
+        size_t len = entry.length() + 1;
         argv[count] = new char[len];
-        std::copy(entry.begin(),entry.end(),argv[count]);
-        argv[count][len-1]='\0';
+        std::copy(entry.begin(), entry.end(), argv[count]);
+        argv[count][len - 1] = '\0';
         count++;
     }
 
-    argv[count]=nullptr;
+    argv[count] = nullptr;
 
     xprintf_message_func = &internal_printf;
     _messages.clear();
@@ -47,7 +47,7 @@ int xd3_main_exec(const std::vector<std::string>& params)
     int ret = xd3_main_cmdline(count, argv);
 
     xprintf_message_func = nullptr;
-    for(int i=0;i<count;i++)
+    for (int i = 0; i < count; i++)
     {
         delete[] argv[i];
     }
